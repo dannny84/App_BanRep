62ca4eac344b89b228633e5e8c29af24f4bf98a5
Danny Londoño <dannny84@gmail.com>
2013-08-24 16:07:52 -0500

    V 1.0

---
+++ www/js/UseJQuery.js
@@ -0,0 +1,52 @@
+function CallService()
+{
+    //Creamos la variable que contiene la url del webservice
+    var webServiceURL = 'http://localhost:8080/WSPrueba/services/Hola?wsdl';
+    //Este es el mensaje SOAP, dentro de las etiquetas <CI>'+ $('#ci').val() +'</CI> hacemos uso de una función JQuery para obtener valor que está en el campo de texto
+    //var soapMessage = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><holaMundo xmlns="http://src"></holaMundo></soap:Body></soap:Envelope>';
+	var soapMessage = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><saludo xmlns="http://src"><nombre>'+ $('#CI').val() +'</nombre></saludo></soap:Body></soap:Envelope>';
+ 
+    //Llamamos a la función AJAX de JQuery
+	
+	if($("#CI").val().length > 0){
+		$.ajax({
+			url: "http://localhost:8080/WSPrueba/services/Hola/saludo", 
+			type: "POST",
+			data: { nombre : $("#CI").val() }, 
+			success: OnSuccess,
+			error: OnError
+		});
+	}else{
+	
+		$.ajax({
+			url: "http://localhost:8080/WSPrueba/services/Hola/holaMundo", 
+			type: "POST",
+			//contentType: "text/xml;charset=UTF-8",
+			//dataType: "xml",
+			data: { "":"" },
+			success: OnSuccess,
+			error:OnError
+		});
+	}
+    return false;
+}
+//Función que se ejecuta si realizó completa la petición
+function OnSuccess(data, status, req)
+{		
+		//Check to see if an object is a plain object (created using "{}" or "new Object").
+		//alert(jQuery.isPlainObject(data));
+		//alert($.isEmptyObject(data)); 
+		//alert(JSON.stringify(data));
+		console.log(data);
+		//$(data).find("return").text();
+		$("#filaRespuesta").css("display","table");
+		$("#respuesta").val(data.getElementsByTagName("ns:return")[0].textContent);
+}
+function OnError(request, status, error)  //Función que se ejecuta si ocurre algún error
+{
+     alert(request.status + " " + request.statusText);
+}
+$(function() {
+    //Evita problemas de cross-domain con JQuery
+    jQuery.support.cors = true;
+});
